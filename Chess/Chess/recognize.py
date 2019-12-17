abf6a3e10d307406d2de826a7e26b9bab92cde44
123Xtom <40460710+123Xtom@users.noreply.github.com>
2019-12-17 17:33:44 +0100

    recognize_move_from_speech aktualizacja

--- Chess/Chess/recognize.py
+++ Chess/Chess/recognize.py
@@ -65,9 +65,20 @@ def recognize_move(transcript):
         return 'blad'
 
 def make_move_from_speech(transcript, board):
-    figures_name = [r'kr[óo]l', r'dama', r'hetman', r'kr[óo]lowa', r'goniec?', r'laufer', r'ko[ńn]', r'skoczek',
-                   r'wie[żz]a', r'pione?k?']
-    figures_sign = ['K', 'Q', 'Q', 'Q', 'B', 'B', 'N', 'N', 'R', 'P']
+    """
+    trascript - text (string)
+    board - chess.Board() object
+
+    return: Move object or -1
+
+    reacts both on figure names (if exact) or coordinates
+    roszada (O-O)/(O-O-O): reacts on 'roszada' word. If both are legal return long first (O-O-O)
+    promotion: search figure name after 'promocja' word. 
+    """
+
+    figures_name = [r'kr[óo]la?', r'dam[ay]', r'hetmana?', r'kr[óo]low[aą]', r'goniec', r'laufe?ra?', r'k?o[ńn]i?e?',
+                   r'skocze?ka?', r'wie[żz][aęe]', r'pione?k?a?', r'jane?ka?']
+    figures_sign = ['K', 'Q', 'Q', 'Q', 'B', 'B', 'N', 'N', 'R', 'P', 'P']
 
     numax = []
     regularnum = [r'1', r'2', r'3', r'4', r'5', r'6', r'7', r'8']
@@ -77,31 +88,54 @@ def make_move_from_speech(transcript, board):
     regularICAO = [r'alfa', r'brav?w?o', r'c?z?h?arlie?', r'delta', r'ec?ho', r'fox?k?s?trot?', r'golf', r'hotel?']
     letterax = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
+    # reakcja na słowo 'roszada'
+    rosz = 0
+    rosz = re.search(r'roszada', transcript)
+    if rosz != 0:
+        try:
+            return board.parse_san('O-O-O')
+        except ValueError:
+            pass
+        try:
+            return board.parse_san('O-O')
+        except ValueError:
+            return -1
+
     command = re.split('na', transcript)
 
+    prom_fig = 0
+    promotion = re.split('promocja', transcript)
 
     if len(command) != 2:
         print('nie wykryto słowa "na"\nproszę spróbować ponownie')
         return -1
-    print(command[0], ' ', command[1])
+    if len(promotion) == 2:
+        print(command[0], ' ', command[1], 'promocja: ', promotion[1])
+    else:
+        print(command[0], ' ', command[1])
 
-    # wykrywanie, czy wypowiedziano nazwe figury
+    # wykrywanie, czy wypowiedziano nazwe figury lub nazwę figury po promocji
     f = 0
     for i in range(len(figures_name)):
         figure = re.search(figures_name[i], command[0])
+        try:
+            prom_fig = re.search(figures_name, promotion[1])
+            prom_fig = figures_sign[i]
+        except IndexError:
+            pass
         try:
             #print(figure.group(0))
             f = figure.group(0)
             f = figures_sign[i]
         except AttributeError:
             pass
-    print(f)
+
+    #print(f)
     # jesli wykryto nazwe figury
     if f != 0:
         for i in range(len(letterax)):
             b = re.search(regularICAO[i], command[1])
             nb = re.search(regularnum[i], command[1])
-            
 
             try:
                 l = b.group(0)
@@ -116,10 +150,14 @@ def make_move_from_speech(transcript, board):
                 pass
 
             try:
-                if f == 'P':
-                    move = letSecond + numSecond
+                if prom_fig == 0:
+                    if f == 'P':
+                        move = letSecond + numSecond
+                    else:
+                        move = f + letSecond + numSecond
                 else:
-                    move = f + letSecond + numSecond
+                    move = letSecond + numSecond + prom_fig
+
                 
                 #print('move: ',move)
                 move = board.parse_san(move)
@@ -161,6 +199,8 @@ def make_move_from_speech(transcript, board):
             except AttributeError:
                 pass
         try:
+            if prom_fig != 0:
+                return board.parse_uci(letFirst + numFirst + letSecond + numSecond + prom_fig)
             return board.parse_uci(letFirst + numFirst + letSecond + numSecond)
         except UnboundLocalError:
             #print("1: nie wykryto prawidlowego ruchu \nproszę spróbować ponownie")
